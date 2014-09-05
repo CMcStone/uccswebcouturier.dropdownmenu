2c6bad67295c4e0efbfa5873104f2571b2be0b2c
Carol McMasters-Stone <cbeck@ucdavis.edu>
2014-09-05 12:56:25 -0700

    intial commit

---
+++ src/uccswebcouturier/dropdownmenu/__init__.py
@@ -0,0 +1,7 @@
+from AccessControl import allow_module
+
+allow_module('uccswebcouturier.dropdownmenu.utils')
+
+
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
